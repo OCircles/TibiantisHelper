34b6396c4a8bfe5af6353fe27749219f4230148d
O_Circles <43825806+OCircles@users.noreply.github.com>
2020-11-13 09:11:23 +0100

    Added main form icon
    Forgot this for v0.8

--- TibiantisHelper/Form_Main.Designer.cs
+++ TibiantisHelper/Form_Main.Designer.cs
@@ -2266,7 +2266,9 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form_Main";
+            this.ShowIcon = false;
             this.Text = "Tibiantis Helper v0.8";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
