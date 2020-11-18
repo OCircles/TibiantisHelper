a377b21b20d1bf52aab890490ee679235aefed7b
O_Circles <43825806+OCircles@users.noreply.github.com>
2020-11-18 06:33:33 +0100

    Updated some throwable fragility

--- TibiantisHelper/DataReader.cs
+++ TibiantisHelper/DataReader.cs
@@ -642,7 +642,6 @@ namespace TibiantisHelper
 
                     if (split.Length >= 2)
                     {
-                        Console.WriteLine(line);
 
                         string first = split[0];
                         string second = split[2];
