c854d2d86aa793b1799702b0b72bce176174e1f7
O_Circles <43825806+OCircles@users.noreply.github.com>
2022-02-09 17:34:05 +0100

    New player group checks for duplicates

--- TibiantisHelper/Tabs/LoginAlert/Tab_LoginAlert.cs
+++ TibiantisHelper/Tabs/LoginAlert/Tab_LoginAlert.cs
@@ -51,6 +51,12 @@ namespace TibiantisHelper.Tabs.LoginAlert
             {
                 if (!string.IsNullOrEmpty(diag.GroupName))
                 {
+                    if (PlayerGroups.Exists(ss => diag.GroupName.Equals(diag.GroupName,StringComparison.OrdinalIgnoreCase)))
+                    {
+                        MessageBox.Show("Duplicate group name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
                     var grp = new TrackedPlayerGroup() { Name = diag.GroupName };
                     PlayerGroups.Add(grp);
                     AddGroupControl(grp);
