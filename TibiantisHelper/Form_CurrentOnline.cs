14588efa40c3650910556f1a9a54cd303a39b762
O_Circles <43825806+OCircles@users.noreply.github.com>
2022-02-09 17:29:13 +0100

    Fixed broken reference

--- TibiantisHelper/Form_CurrentOnline.cs
+++ TibiantisHelper/Form_CurrentOnline.cs
@@ -125,7 +125,7 @@ namespace TibiantisHelper
                     });
                     ct.Items.Add("Name to Clipboard", null, (s,ee) => { Utility.StringToClipboard(player.Name); });
                     ct.Items.Add("Add to Login Alert", null, (s, ee) => {
-                        Form_Main.Form.tab_LoginAlert1.ImportPlayers( new string[] { player.Name }.ToList() );
+                        Form_Main.Form.tab_LoginAlert1.Import( new string[] { player.Name }.ToList() );
                         Form_Main.Form.tabControl1.SelectedTab = Form_Main.Form.tabPage_LoginAlert;
                     });
 
